ed41b982583313c405aeda572857a21c9abd9fb4
Elena Bukhmastova <lenbuh74@mail.ru>
2019-03-09 22:58:48 +0300

    2

---
+++ Lab1/js/script.js
@@ -0,0 +1,9 @@
+shortMenu.onclick = function myFunction() {
+	var x = document.getElementById("myMenu")
+
+	if(x.className === "menu") {
+		x.className += " responsive";
+		} else{
+			x.className = "menu";
+		}
+}
