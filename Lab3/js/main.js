369049af08b1cb77065dbb6fac761e4fa2e0574f
Elena Bukhmastova <lenbuh74@mail.ru>
2019-04-01 17:23:58 +0300

    update

--- Lab3/js/main.js
+++ Lab3/js/main.js
@@ -1,12 +1,15 @@
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
-
-var image = new Image();   
-
-image.src = 'https://source.unsplash.com/collection/1127163/300x200';
-
-
-
-image.onload = function() {
-   ctx.drawImage(image, 0, 0, 300, 200);
+window.onload = function(){
+     var canvas = document.getElementById('canvas');
+     var context = canvas.getContext("2d");
+     var imageObj = new Image();
+     imageObj.src = 'https://source.unsplash.com/collection/1127163/300x200'; 
+     imageObj.onload = function(){
+        for (var i=0;i<4;i++){
+        for (var j=0;j<3;j++){
+        context.drawImage(imageObj,j*300,i*200);
+      }
+    }
+    context.font = "30pt Lora";
+    context.fillText("My TEXT!", 50, 75);
+     };    
 };
