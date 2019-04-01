4faf2136f62b2ab7f852ffbfaa82dbbdc7dd2571
Elena Bukhmastova <lenbuh74@mail.ru>
2019-04-01 20:25:32 +0300

    update collage

--- Lab3/js/main.js
+++ Lab3/js/main.js
@@ -5,22 +5,40 @@ window.onload = function(){
 
     var canvas = document.getElementById('canvas'); //find new canvas we created
     var context = canvas.getContext('2d');
-    canvas.width = 1000;
-    canvas.height = 1000;
+    canvas.width = 600;
+    canvas.height = 400;
 
-    var imageObj = new Image();
-    imageObj.src = 'https://source.unsplash.com/collection/1127160/300x200';
+    x1 = Math.floor(Math.random() * (300 - 100 + 1)) + 100;
+    x2 = 600 - x1;
+    x3 = Math.floor(Math.random() * (300 - 100 + 1)) + 100;
+    x4 = 600 - x3;
+    y = Math.floor(Math.random() * (210 - 100 + 1)) + 100;
+    y2= 400 - y;
     
-    var i;
-    var j;
+    var imageObj = new Image();
+    imageObj.src = 'https://source.unsplash.com/collection/123/'+x1+'x'+y;
+
+    var imageObj2 = new Image();
+    imageObj2.src = 'https://source.unsplash.com/collection/145/'+x2+'x'+y;
+
+    var imageObj3 = new Image();
+    imageObj3.src = 'https://source.unsplash.com/collection/112/'+x3+'x'+y2;
+
+    var imageObj4 = new Image();
+    imageObj4.src = 'https://source.unsplash.com/collection/136/'+x4+'x'+y2;
+
     imageObj.onload = function(){
-        for (i = 0; i < 2; i++){
-            for (j = 0; j < 2; j++){
-                context.drawImage(imageObj,j*300,i*200);
-      }
-    }
-    context.font = "30pt Lora";
-    context.fillText("My TEXT!", 50, 75);
-     };    
+        context.drawImage(imageObj,0,0);
+        imageObj2.onload = function(){
+            context.drawImage(imageObj2,x1,0);
+            imageObj3.onload = function(){
+                context.drawImage(imageObj3,0,y);
+                imageObj4.onload = function(){
+                    context.drawImage(imageObj4,x3,y);
+                };
+            };
+        };
+    };
+    
 };
 
