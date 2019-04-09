b73cbb0e2ccddb92714ccc7cadbdecd6b416234b
Elena Bukhmastova <lenbuh74@mail.ru>
2019-04-09 16:57:13 +0300

    update

--- Lab3/js/main.js
+++ Lab3/js/main.js
@@ -1,73 +1,88 @@
 window.onload = function(){
    
-    function showImg (x1, x2, x3, x4, y, y2) {
+    
+    function showImg () {
 
     var imageObj = new Image();
     imageObj.src = 'https://source.unsplash.com/collection/123/'+x1+'x'+y;
+    imageObj.crossOrigin = 'anonymous';
 
     var imageObj2 = new Image();
     imageObj2.src = 'https://source.unsplash.com/collection/145/'+x2+'x'+y;
+    imageObj2.crossOrigin = 'anonymous';
 
     var imageObj3 = new Image();
     imageObj3.src = 'https://source.unsplash.com/collection/112/'+x3+'x'+y2;
+    imageObj3.crossOrigin = 'anonymous';
 
     var imageObj4 = new Image();
     imageObj4.src = 'https://source.unsplash.com/collection/136/'+x4+'x'+y2;
+    imageObj4.crossOrigin = 'anonymous';
 
-        imageObj.onload = function(){
-        context.drawImage(imageObj,0,0);
-            imageObj2.onload = function(){
-                context.drawImage(imageObj2,x1,0);
-                imageObj3.onload = function(){
-                    context.drawImage(imageObj3,0,y);
-                    imageObj4.onload = function(){
-                        context.drawImage(imageObj4,x3,y);
-                        
-                    };
-                };
-            };     
-        }; 
-
-    var a = document.createElement('a');
-    a.id = 'link';
-    a.innerHTML = 'Скачать картинку';
-    document.body.appendChild(a);
-    
-    link.setAttribute('download', 'image.png');
-    link.setAttribute('href', canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"));  
+    imageObj.onload = function () {
+    context.drawImage(imageObj,0,0);
+    };
+    imageObj2.onload = function () {
+    	 context.drawImage(imageObj2,x1,0);
+    };
+    imageObj3.onload = function () {
+        context.drawImage(imageObj3,0,y);
+    };
+    imageObj4.onload = function () {
+        context.drawImage(imageObj4,x3,y);
+    };
+       
     }
-    
 
-    function showQuote() {
+   function showQuote(source) {
 
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://cors-anywhere.herokuapp.com/https://api.forismatic.com/api/1.0/?method=getQuote&lang=ru&format=text', true);
-    text = xhr.responseText;
-    context.fillText('eto test' + text, 50, 50);
-    xhr.onload = function () {
-        if (xhr.status !== 200) {
+   	setTimeout( function(){
+    xhr = new XMLHttpRequest();
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+            data = xhr.responseText;
+            context.fillStyle = "rgba(0, 0, 0, 0.3)";
+            context.fillRect(0,0,600,400);
+            context.fillStyle = 'white';
+            context.textAlign = "center";
+            context.font = '16px Room';
+            context.fillText(data, 300, 200);
+            console.log(context.measureText(data).width);
+            
+        }
+        else {
             var error = xhr.status + ': ' + xhr.sstatusText;
-            context.fillText('eto error' + error, 100,200);
-        } else {
-            var quote = xhr.responseText;
-            context.fillText('eto quote' + quote, 40, 100); 
-            xhr.send(); 
-            context.fillText('proverka', 100, 100);
+            console.log(error);
         }
     }
-    xhr.onerror = function() {
-        var error2 = "Ошибка: " + this.src;
-        context.fillText(error2, 40,100);
-        };
-     context.fillText('proverka vne onload', 100, 100);
-   };
+    xhr.open('GET', source, true);
+    xhr.send(null);
+  }, 2500 );
+  };
 
+
+    function saveCanvasAsImageFile(){
+    	var imageData = canvas.toDataURL();
+    	var image = new Image();
+    	image.src = imageData;
+    	var link = document.createElement("a");
+    	link.setAttribute("href", image.src);
+    	link.setAttribute("download", "collage_lab3");
+    	link.click();
+	}
 
  
     var canv = document.createElement('canvas'); // creates new canvas element
     canv.id = 'canvas'; // gives canvas id
     document.body.appendChild(canv); // adds the canvas to the body element
     canv.style.position = "relative";
+    canv.onclick = function () {
+        var dataURL = canv.toDataURL("image/png");
+        var link = document.createElement("a");
+        link.href = dataURL;
+        link.download = "image-lab3.png";
+        link.click();
+    };
 
     var canvas = document.getElementById('canvas'); //find new canvas we created
     var context = canvas.getContext('2d');
@@ -77,16 +92,35 @@ window.onload = function(){
     context.fillStyle = "rgba(0,0,0,0.3)";
     context.fillRect(0,0,600,400);
 
-    x1 = Math.floor(Math.random() * (300 - 100)) + 100;
+    x1 = Math.floor(Math.random() * (400 - 100)) + 100;
     x2 = 600 - x1;
-    x3 = Math.floor(Math.random() * (300 - 100)) + 100;
+    x3 = Math.floor(Math.random() * (400 - 100)) + 100;
     x4 = 600 - x3;
     y = Math.floor(Math.random() * (210 - 100)) + 100;
     y2= 400 - y;
     
+    var URL = 'https://cors-anywhere.herokuapp.com/https://api.forismatic.com/api/1.0/?method=getQuote&format=text&lang=ru';   
+    showImg();
+    showQuote(URL);
 
-    showImg(x1, x2, x3, x4, y, y2);
-    showQuote();         
+	var br = document.createElement('br'); 
+    document.body.appendChild(br);
+
+    var btn_save = document.createElement('button');
+    document.body.appendChild(btn_save);
+    btn_save.innerHTML = 'Save picture';
+    btn_save.color = 'yellow';
+    btn_save.onclick = function () {
+    	saveCanvasAsImageFile();
+    }
+
+    var btn_reload = document.createElement('button'); 
+    document.body.appendChild(btn_reload);
+    btn_reload.innerHTML = 'New picture';
+    btn_reload.onclick = function () {
+    	window.location.reload();
+    }   
+
 };
 
 
