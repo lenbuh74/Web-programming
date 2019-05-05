5fef164a1ea493f16d0f3a73b6ce0ad02be96562
Elena Bukhmastova <lenbuh74@mail.ru>
2019-05-05 11:31:55 +0300

    v 05.05.1

--- Lab5/server/src/index.js
+++ Lab5/server/src/index.js
@@ -1,1 +1,16 @@
-console.log(“Hello World”);
+//console.log("Hello World");
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+const config = require('./config/config')
+
+const app = express()
+
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.listen(process.env.PORT || config.port,
+  () => console.log(`Server start on port ${config.port} ...`))
