5ed455b6dee77b91b09f0b000760fb66ae1c3844
Elena Bukhmastova <lenbuh74@mail.ru>
2019-05-05 17:38:58 +0300

    v 05.05.2

--- Lab5/server/src/index.js
+++ Lab5/server/src/index.js
@@ -12,5 +12,14 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
+app.get('/posts', (req, res) => {
+  res.send(
+    [{
+      title: "Hello World!",
+      description: "Hi there! How are you?"
+    }]
+  )
+})
+
 app.listen(process.env.PORT || config.port,
   () => console.log(`Server start on port ${config.port} ...`))
